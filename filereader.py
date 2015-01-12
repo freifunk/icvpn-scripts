ecbb0e03846f3d9f894839a6ab5c77d0f9a1ccb9
Jan-Philipp Litza <janphilipp@litza.de>
2015-01-12 21:10:50 +0100

    Split off file file reading part, improve input checking

---
+++ filereader.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+import os
+import sys
+import yaml
+
+
+def _default_error_handler(community):
+    print("Invalid YAML: %s" % community, file=sys.stderr)
+
+
+def get_communities_data(srcdir, exclude,
+                         error_handler=_default_error_handler):
+    for fname in sorted(list(set(os.listdir(srcdir)) - set(exclude))):
+        if fname.startswith("."):
+            continue
+
+        if fname.startswith("README"):
+            continue
+
+        fpath = os.path.join(srcdir, fname)
+        if os.path.isfile(fpath):
+            with open(fpath) as f:
+                try:
+                    data = yaml.load(f)
+                except yaml.YAMLError:
+                    error_handler(fname)
+                    continue
+            yield (fname, data)
